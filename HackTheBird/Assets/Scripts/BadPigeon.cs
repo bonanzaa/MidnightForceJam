223c9adb701bb870020af2f78123f993cf4d779c
Daniel Salas <salasdaniel1998@gmail.com>
2021-05-08 18:49:51 +0200

    Daniel: The Revival of the Pigeon

--- HackTheBird/Assets/Scripts/BadPigeon.cs
+++ HackTheBird/Assets/Scripts/BadPigeon.cs
@@ -4,7 +4,10 @@ public class BadPigeon : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
-
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
     private void Update()
     {
         transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);
