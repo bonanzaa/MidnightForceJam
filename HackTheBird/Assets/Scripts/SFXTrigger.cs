e21af8bb9090fa04b9415867485dac27ce3259fe
Pete <pete.kolpakov@gmail.com>
2021-05-08 15:37:34 +0200

    implemented SFX in test scene

---
+++ HackTheBird/Assets/Scripts/SFXTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class SFXTrigger : MonoBehaviour
+    {
+        private AudioSource clip;
+
+        private void Awake()
+        {
+            clip = GetComponent<AudioSource>();
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayClip();
+            }
+        }
+        private void PlayClip()
+        {
+            clip.Play();
+        }
+    }
+}
